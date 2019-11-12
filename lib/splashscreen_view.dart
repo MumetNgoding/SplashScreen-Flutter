3fde2c3c1cdbe8d2e24433f4067d37417ed196bf
MumetNgoding <nugroho31.com@gmail.com>
2019-11-11 16:15:53 -0800

    SplashScreen - Flutter

---
+++ lib/splashscreen_view.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/home_view.dart';
+import 'dart:async';
+
+class SplashScreenPage extends StatefulWidget {
+  @override
+  _SplashScreenPageState createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startSplashScreen();
+  }
+
+  startSplashScreen() async {
+    var duration = const Duration(seconds: 5);
+    return Timer(duration, () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) {
+          return Homepage();
+        })
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff329cef),
+      body: Center(
+        child: Image.asset(
+          "img/splash.png",
+          // width: 200.0,
+          // height: 100.0,
+        ),
+      ),
+    );
+  }
+}
